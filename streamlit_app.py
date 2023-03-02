1b0a898aea6957102d0e56da375ae1316fedec93
Malo-Aymes <120594447+Malo-Aymes@users.noreply.github.com>
2023-03-02 16:55:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
-"""import mysql.connector
+import mysql.connector
 import streamlit as st
 
 # Initialize connection.
-# Uses st.cache_resource to only run once.
-@st.cache_resource
 def init_connection():
     config = st.secrets["tidb"]
     return mysql.connector.connect(
@@ -35,36 +33,4 @@ def write(s,c):
 button = st.button("Write")
 
 if user_input and button:
-         write(user_input,option)"""
-
-import mysql.connector as sql
-import streamlit as st
-
-# Initialize connection.
-def init_connection():
-    config = st.secrets["tidb"]
-    return sql.connect(
-        host=config["host"],
-        port=config["port"],
-        user=config["user"],
-        password=config["password"],
-        database=config["database"],
-        ssl_ca=config["ssl-ca"]
-    )
-
-conn = init_connection()
-
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-    
-button = st.button("O")
-
-if button:
-    rows = run_query("SELECT * from classification;")
-
-    st.write(rows)
+         write(user_input,option)
