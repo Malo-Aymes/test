67c14daa2225ca802c262fad5afa3a49cb63cd50
Malo-Aymes <120594447+Malo-Aymes@users.noreply.github.com>
2023-03-02 16:56:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,8 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
+user_input = st.textbox('Question:')    
+    
 option = st.selectbox('Class :', ('Weather', 'Clock', 'Calendar', 'Map', 'Phone', 'Email', 'Calculator', 'Translator', 'Web search', 'Social media', 'Small talk', 'Message', 'Reminders', 'Music'))
 
 
