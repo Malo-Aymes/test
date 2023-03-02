c96df74f8d672ba78d77caf482e070d5d2483d7d
Malo-Aymes <120594447+Malo-Aymes@users.noreply.github.com>
2023-03-02 15:59:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+"""import streamlit as st
 
 user_input = st.text_area("Text :")
 
@@ -9,4 +9,38 @@ def write(s):
 button = st.button("Write")
 
 if user_input and button:
-         write(user_input)
+         write(user_input)"""
+
+import MySQLdb
+import streamlit as st
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    config = st.secrets["tidb"]
+    return MySQLdb.connect(
+        host=config["host"],
+        port=config["port"],
+        user=config["user"],
+        password=config["password"],
+        database=config["database"],
+        ssl_mode="VERIFY_IDENTITY",
+        ssl={"ca": config["ssl-ca"]}
+    )
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from mytable;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
